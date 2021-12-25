b7253dff4a8f91d71c517640f8f53154b1e2ca20
George Richards <georgejjrichards@gmail.com>
2021-12-25 17:58:06 +0000

    all

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,25 @@
 import React from "react"
+import { App as SendbirdApp } from "sendbird-uikit"
+import "sendbird-uikit/dist/index.css"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function App() {
+  const { APP_ID, USER_ID, NICKNAME, THEME } = {
+    APP_ID: "2D7B4CDB-932F-4082-9B09-A1153792DC8D",
+    USER_ID: "sendbirdian-200720",
+    NICKNAME: "Sendbirdian84",
+    THEME: "light",
+  }
+
+  return (
+    <div className="customized-app">
+      <SendbirdApp
+        appId={APP_ID}
+        userId={USER_ID}
+        nickname={NICKNAME}
+        theme={THEME}
+        useReaction={true}
+        useMessageGrouping={true}
+      />
+    </div>
+  )
 }
